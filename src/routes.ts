64e47f25cd70444189ce8a4d34b4e86d7ae9a975
Rian Marlon <rianmarlon20@yahoo.com>
2020-08-04 20:48:18 -0300

    Adicionando o método post no recurso classes no arquivo routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,59 @@
 import express from 'express';
+import db from './database/connection';
+import convertHourToMinute from './utils/convertHourToMinute';
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' });
+interface ScheduleItem {
+  week_day: number,
+  from: string,
+  to: string
+}
+
+routes.post('/classes', async (request, response) => {
+  const {
+    name,
+    avatar,
+    whatsapp,
+    biography,
+    subject,
+    cost,
+    schedules
+  } = request.body;
+
+  const transaction = await db.transaction();
+
+  const insertedUsersIds = await transaction('users').insert({
+    name,
+    avatar,
+    whatsapp,
+    biography
+  });
+
+  const id_user = insertedUsersIds[0];
+
+  const insertedClassesIds = await transaction('classes').insert({
+    subject,
+    cost,
+    id_user
+  });
+
+  const id_class = insertedClassesIds[0];
+
+  const classSchedules = schedules.map((scheduleItem: ScheduleItem) => {
+    return {
+      week_day: scheduleItem.week_day,
+      from: convertHourToMinute(scheduleItem.from),
+      to: convertHourToMinute(scheduleItem.to),
+      id_class
+    };
+  });
+
+  await transaction('class_schedules').insert(classSchedules);
+
+  await transaction.commit();
+
+  return response.send();
 });
 
 export default routes;
