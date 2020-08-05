e5d24fe78fdc0816e4fa4991fe9ba7ec1911dead
Rian Marlon <rianmarlon20@yahoo.com>
2020-08-05 00:36:29 -0300

    Adicionando um try/catch no método post do recurso classes

--- src/routes.ts
+++ src/routes.ts
@@ -23,37 +23,45 @@ routes.post('/classes', async (request, response) => {
 
   const transaction = await db.transaction();
 
-  const insertedUsersIds = await transaction('users').insert({
-    name,
-    avatar,
-    whatsapp,
-    biography
-  });
+  try {
+    const insertedUsersIds = await transaction('users').insert({
+      name,
+      avatar,
+      whatsapp,
+      biography
+    });
 
-  const id_user = insertedUsersIds[0];
+    const id_user = insertedUsersIds[0];
 
-  const insertedClassesIds = await transaction('classes').insert({
-    subject,
-    cost,
-    id_user
-  });
+    const insertedClassesIds = await transaction('classes').insert({
+      subject,
+      cost,
+      id_user
+    });
 
-  const id_class = insertedClassesIds[0];
+    const id_class = insertedClassesIds[0];
 
-  const classSchedules = schedules.map((scheduleItem: ScheduleItem) => {
-    return {
-      week_day: scheduleItem.week_day,
-      from: convertHourToMinute(scheduleItem.from),
-      to: convertHourToMinute(scheduleItem.to),
-      id_class
-    };
-  });
+    const classSchedules = schedules.map((scheduleItem: ScheduleItem) => {
+      return {
+        week_day: scheduleItem.week_day,
+        from: convertHourToMinute(scheduleItem.from),
+        to: convertHourToMinute(scheduleItem.to),
+        id_class
+      };
+    });
 
-  await transaction('class_schedules').insert(classSchedules);
+    await transaction('class_schedules').insert(classSchedules);
 
-  await transaction.commit();
+    await transaction.commit();
 
-  return response.send();
+    return response.status(201).send();
+  } catch (err) {
+    transaction.rollback();
+
+    return response.status(400).json({
+      error: 'Unexpected error while creating new class'
+    });
+  }
 });
 
 export default routes;
