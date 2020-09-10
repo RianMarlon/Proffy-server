f41d765cd3a2026bc81741da21f1e0ea1745c5ff
Rian Marlon <rianmarlon20@yahoo.com>
2020-09-09 23:27:32 -0300

    Adicionando o middleware de autenticação nas rotas necessárias

--- src/routes.ts
+++ src/routes.ts
@@ -2,13 +2,17 @@ import express from 'express';
 import ClassesControllers from './controllers/ClassesControllers';
 import ConnectionsControllers from './controllers/ConnectionsControllers';
 
+import authenticate from './middlewares/auth';
+
 const routes = express.Router();
 const classesControllers = new ClassesControllers();
 const connectionsControllers = new ConnectionsControllers();
 
+routes.all('/classes', authenticate);
 routes.get('/classes', classesControllers.getWithSchedules);
 routes.post('/classes', classesControllers.insert);
 
+routes.all('/connections', authenticate);
 routes.get('/connections', connectionsControllers.getCount);
 routes.post('/connections', connectionsControllers.insert);
 
