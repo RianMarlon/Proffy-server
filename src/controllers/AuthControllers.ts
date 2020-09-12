7ff3a6d3161be2cadd1aad8583a6798043abef47
Rian Marlon <rianmarlon20@yahoo.com>
2020-09-12 16:00:15 -0300

    Adicionando o método forgotPassword na classe AuthControllers da pasta controllers

--- src/controllers/AuthControllers.ts
+++ src/controllers/AuthControllers.ts
@@ -5,6 +5,9 @@ import { promisify } from 'util';
 
 import { existOrError } from '../utils/validate';
 import db from '../database/connection';
+import emailService from '../modules/emailService';
+
+const { email: sender } = require('../config/emailService.json');
 const { authSecret } = require('../../.env');
 
 export default class AuthControllers {
@@ -70,6 +73,47 @@ export default class AuthControllers {
     } catch (err) {
       return response.status(200).json({
         isTokenValid: false,
+      });
+    }
+  }
+
+  async forgotPassword(request: Request, response: Response) {
+    const { email } = request.body;
+
+    try {
+      const userByEmail = await db('users')
+        .select('users.id')
+        .where('email', '=', email)
+        .first();
+
+      existOrError(userByEmail, 'Não existe usuário com o e-mail informado!');
+
+      const payload = {
+        id: userByEmail.id
+      }
+
+      const token = jwt.sign({ ...payload }, authSecret, {
+        expiresIn: '30m',
+      });
+
+      emailService.sendMail({
+        to: email as string,
+        from: sender,
+        template: 'auth/forgotPassword',
+        context: { token },
+      }, (err: any) => {
+        if (err) {
+          return response.status(500).json({
+            error: 'Não foi possível enviar o e-mail!'
+          });
+        }
+
+        return response.status(200).send();
+      });
+
+    } catch(err) {
+      response.status(400).json({
+        error: err,
       });
     }
   }
