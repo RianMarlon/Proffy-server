be3af08fb2f80d555cbe499df8c227f2ad5e04aa
Rian Marlon <rianmarlon20@yahoo.com>
2020-09-20 15:21:51 -0300

    Adicionando a função compressImage na pasta utils

---
+++ src/utils/compressImage.ts
@@ -0,0 +1,77 @@
+
+import fs from 'fs';
+import sharp from 'sharp';
+
+import URL_BACKEND from '../config/url';
+
+const compressImage = (file: any, size: number) => {
+  const pathSplitted = file.path.split('.');
+  const newPath = pathSplitted.slice(0, pathSplitted.length - 1) + '.png';
+
+  if (file.path == newPath) {
+    return sharp(file.path)
+      .resize(size)
+      .png({
+        quality: 80
+      })
+      .toBuffer()
+      .then((data) => {    
+        fs.writeFile(newPath, data, (err) => {
+          if(err){
+            throw err;
+          }
+        });
+
+        const fileNameSplitted = file.filename.split('.');
+
+        const newFileName = fileNameSplitted.slice(0, pathSplitted.length - 1)
+          + '.png';
+
+        const urlImage = `${URL_BACKEND}/files/${newFileName}`;
+
+        return {
+          id: newFileName,
+          path: newPath,
+          url: urlImage,
+        };
+    });
+  }
+
+  return sharp(file.path)
+    .resize(size)
+    .toFormat('png')
+    .png({
+      quality: 80
+    })
+    .toBuffer()
+    .then(data => {
+      fs.access(file.path, (err) => {
+        if (!err) {
+          fs.unlink(file.path, (err) => {
+            if(err) console.log(err);
+          });
+        }
+      });
+      
+      fs.writeFile(newPath, data, (err) => {
+        if(err){
+          throw err;
+        }
+      });
+
+      const fileNameSplitted = file.filename.split('.');
+
+      const newFileName = fileNameSplitted.slice(0, pathSplitted.length - 1)
+        + '.png';
+
+      const urlImage = `${URL_BACKEND}/files/${newFileName}`;
+
+      return {
+        id: newFileName,
+        path: newPath,
+        url: urlImage,
+      };
+  });
+}
+
+export default compressImage;
