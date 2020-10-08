dcf56e7d5436aa9eba7647f440027e77e7106f53
Rian Marlon <rianmarlon20@yahoo.com>
2020-10-08 13:54:10 -0300

    Adicionando a migration de criação da tabela favorites na pasta migrations

---
+++ src/database/migrations/20201005132100_create_table_favorites.ts
@@ -0,0 +1,25 @@
+import Knex from 'knex';
+
+export async function up(knex: Knex) {
+  return knex.schema.createTable('favorites', (table) => {
+    table.increments('id').primary();
+
+    table.integer('id_user')
+      .notNullable()
+      .references('id')
+      .inTable('users')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+
+    table.integer('id_class')
+      .notNullable()
+      .references('id')
+      .inTable('classes')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+  });
+}
+
+export async function down(knex: Knex) {
+  return knex.schema.dropTable('favorites');
+}
