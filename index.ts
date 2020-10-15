edc1ae73ffc022a8168d70a652ccea2793d7d528
Rian Marlon <rianmarlon20@yahoo.com>
2020-10-15 17:28:16 -0300

    Alterando o arquivo index.ts

--- index.ts
+++ index.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
+
 import routes from './src/routes';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+app.use('/files', express.static(path.resolve(__dirname, 'public', 'files')));
 app.use(routes);
 
 app.listen(process.env.PORT || 3333);
